2ccf30f140ba64b3bbbedb8492a7ec295accd386
AshkanTavangar <ashkantavangar7@gmail.com>
2023-02-02 21:17:39 +0330

    Add some details

--- linkedList.js
+++ linkedList.js
@@ -1,24 +1,25 @@
 class Node {
-    constructor(value){
-        this.value = value;
-        this.next= null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
-class linkedList{
-    constructor() {
-        this.head = null;
-        this.size= 0;
-    }
+class linkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
 
-    isEmpty(){
-        return this.size === 0;
-    }
+  isEmpty() {
+    return this.size === 0;
+  }
 
-    getSize(){
-        return this.size;
-    }
+  getSize() {
+    return this.size;
+  }
 }
 
 const list = new linkedList();
-console.log(list.isEmpty());
+console.log("Is it empty? ", list.isEmpty());
+console.log("Size of Linked list : ", list.getSize());
