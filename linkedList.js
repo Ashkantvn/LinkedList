001ca62c85bba0f867c3aec89b05f7007d0f1811
AshkanTavangar <ashkantavangar7@gmail.com>
2023-02-02 21:11:43 +0330

    Create Node and basic linked list classes

---
+++ linkedList.js
@@ -0,0 +1,24 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next= null;
+    }
+}
+
+class linkedList{
+    constructor() {
+        this.head = null;
+        this.size= 0;
+    }
+
+    isEmpty(){
+        return this.size === 0;
+    }
+
+    getSize(){
+        return this.size;
+    }
+}
+
+const list = new linkedList();
+console.log(list.isEmpty());
