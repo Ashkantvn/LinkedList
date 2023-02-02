4c49cee266c52977283872313cbc550980fc4fe4
AshkanTavangar <ashkantavangar7@gmail.com>
2023-02-02 22:05:40 +0330

    Refactor codes for testing linked list

--- linkedList.js
+++ linkedList.js
@@ -19,56 +19,42 @@ class linkedList {
     return this.size;
   }
 
-  prepend(value){
+  prepend(value) {
     const node = new Node(value);
-    if(this.isEmpty()){
-        this.head = node;
-    }else {
-        node.next = this.head;
-        this.head = node;
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
     }
     this.size++;
   }
 
-  print(){
-    if(this.isEmpty()){
-       console.log("linked list is empty!");
-    }else{
-        let currentNode  =  this.head;
-        while(currentNode){
-            console.log(currentNode.value);
-            currentNode = currentNode.next;
-        }
+  print() {
+    if (this.isEmpty()) {
+      console.log("linked list is empty!");
+    } else {
+      let currentNode = this.head;
+      while (currentNode) {
+        console.log(currentNode.value);
+        currentNode = currentNode.next;
+      }
     }
   }
 }
 
-
 ////codes for testing linked list
 const list = new linkedList();
-console.log("Is it empty? ", list.isEmpty());
-console.log("Size of Linked list : ", list.getSize());
-console.log("All values of linked list : ");
-list.print();
-console.log(" ");
-
-list.prepend("Data");
-console.log("Size of linked list after prepend a node : " , list.getSize());
-console.log("Now ,is it empty? ", list.isEmpty());
-console.log("All values of linked list : ");
-list.print();
-console.log(" ");
-
-list.prepend("Data1");
-console.log("Size of linked list after prepend a node : " , list.getSize());
-console.log("Now ,is it empty? ", list.isEmpty());
-console.log("All values of linked list : ");
-list.print();
-console.log(" ");
-
-list.prepend("Data2");
-console.log("Size of linked list after prepend a node : " , list.getSize());
-console.log("Now ,is it empty? ", list.isEmpty());
-console.log("All values of linked list : ");
-list.print();
-console.log(" ");
+for (let i = 0; i < 4; i++) {
+  if (i === 0) {
+    console.log("Is it empty? ", list.isEmpty());
+    console.log("Size of Linked list : ", list.getSize());
+  } else {
+    list.prepend(`Data${i}`);
+    console.log("Size of linked list after prepend a node : ", list.getSize());
+    console.log("Now ,is it empty? ", list.isEmpty());
+  }
+  console.log("All values of linked list : ");
+  list.print();
+  console.log(" ");
+}
